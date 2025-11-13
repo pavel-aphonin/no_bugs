cddec1dcf41c67dea2e9d69929f30c9846286a9a
Pavel Aphonin <pavel.aphonin@gmail.com>
2025-11-13 18:54:44 +0300

    Home task # 8: resolve LinkedHashMap tasks

---
+++ src/Task_8/LinkedHashMap/UserViewHistory.java
@@ -0,0 +1,27 @@
+package Task_8.LinkedHashMap;
+
+import java.util.*;
+
+public class UserViewHistory {
+    private final Map<String, String> history;
+
+    public UserViewHistory() {
+        this.history = new LinkedHashMap<>();
+    }
+
+    public void printViewHistory() {
+        System.out.println(history);
+    }
+
+    public void addView(String url, String pageName) {
+        if (history.size() < 9) {
+            history.put(url, pageName);
+        } else {
+            Iterator<String> iterator = history.keySet().iterator();
+            String removableElement = iterator.next();
+            history.remove(removableElement);
+
+            history.put(url, pageName);
+        }
+    }
+}
