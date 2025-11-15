0a3113ebd88d47ce6cc956d5bcd2ae759846dbef
Pavel Aphonin <pavel.aphonin@gmail.com>
2025-11-15 16:12:52 +0300

    Home task # 8: resolve ArrayDeque tasks

---
+++ src/Task_8/ArrayDeque/Clients.java
@@ -0,0 +1,31 @@
+package Task_8.ArrayDeque;
+
+import java.util.*;
+
+public class Clients {
+    private final ArrayDeque<String> clients;
+
+    public Clients() {
+        this.clients = new ArrayDeque<>();
+    }
+
+    public void addClientFirst(String client) {
+        clients.addFirst(client);
+    }
+
+    public void addClientLast(String client) {
+        clients.addLast(client);
+    }
+
+    public void removeFirstClient() {
+        clients.pollFirst();
+    }
+
+    public void removeLastClient() {
+        clients.pollLast();
+    }
+
+    public void print() {
+        System.out.println(clients);
+    }
+}
