ca851ffef0c6a5ba84a0f667a5e6b537bdec08b4
Pavel Aphonin <pavel.aphonin@gmail.com>
2025-11-15 14:27:19 +0300

    Home task # 8: resolve TreeMap tasks

---
+++ src/Task_8/TreeMap/Numbers.java
@@ -0,0 +1,23 @@
+package Task_8.TreeMap;
+
+import java.util.*;
+
+public class Numbers {
+    private final TreeMap<Integer, Integer> numbers;
+
+    public Numbers() {
+        this.numbers = new TreeMap<>();
+    }
+
+    public void addNumber(Integer number) {
+        numbers.put(number, number);
+    }
+
+    public void findMin() {
+        System.out.println(numbers.firstEntry().getValue());
+    }
+
+    public void findMax() {
+        System.out.println(numbers.lastEntry().getValue());
+    }
+}
